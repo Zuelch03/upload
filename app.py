07b0f74f0a71e1ff43e8394a0585e1c1468cb439
Zuelch03 <gregzuelch316@gmail.com>
2023-08-08 13:55:32 -0700

    commit

--- app.py
+++ app.py
@@ -1,36 +1,40 @@
-from flask import Flask, request, send_file
-import pandas as pd
+from flask import Flask, request, render_template, send_file
+import pandas as pd 
 import re
 
 app = Flask(__name__)
 
-@app.route('/process', methods=['POST'])  
+@app.route('/')
+def home():
+    return render_template('upload.html') 
+
+@app.route('/process', methods=['POST'])
 def process_csv():
 
-  file = request.files['file']
-  
-  df = pd.read_csv(file)
+    csv_file = request.files['file']
+    df = pd.read_csv(csv_file)
 
-  # Extract questions and responses
-  df['Q&A'] = df['add_text'].apply(extract_qa)
-  
-  # Explode list to rows
-  qa_df = df['Q&A'].explode().apply(pd.Series) 
-  qa_df.columns = ['Question', 'Response']
+    # Extract questions and answers
+    df['Q&A'] = df['add_text'].apply(extract_qa)
 
-  # Convert DataFrame to CSV bytes
-  csv_bytes = qa_df.to_csv().encode()
-  
-  return send_file(
-        io.BytesIO(csv_bytes),
+    # Explode to rows
+    qa_df = df['Q&A'].explode().apply(pd.Series)
+    qa_df.columns = ['Question', 'Answer']
+
+    # Convert to CSV
+    csv_bytes = qa_df.to_csv().encode()
+
+    # Return CSV file
+    return send_file(
+        io.BytesIO(csv_bytes), 
         mimetype='text/csv',
         as_attachment=True,
         attachment_filename='processed.csv'
     )
 
 def extract_qa(text):
-  pattern = r'\[Q\d+\. (.*)\]\s+(.*)'
-  return re.findall(pattern, text, re.DOTALL)
+    pattern = r'\[Q\d+\. (.*)\]\s+(.*)'
+    return re.findall(pattern, text, re.DOTALL)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
