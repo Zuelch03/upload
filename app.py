2f1dce858ece0ae57f13dc2233605a9bec498d57
Zuelch03 <gregzuelch316@gmail.com>
2023-08-08 13:59:34 -0700

    commit

--- app.py
+++ app.py
@@ -1,40 +1,30 @@
-from flask import Flask, request, render_template, send_file
-import pandas as pd 
+from flask import Flask, request, render_template, redirect, url_for, send_file
+import pandas as pd
 import re
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('upload.html') 
+    if request.method == 'POST':
+        return redirect(url_for('process_csv'))
 
-@app.route('/process', methods=['POST'])
+    return render_template('upload.html')
+
+@app.route('/process') 
 def process_csv():
-
     csv_file = request.files['file']
     df = pd.read_csv(csv_file)
 
-    # Extract questions and answers
-    df['Q&A'] = df['add_text'].apply(extract_qa)
+    # Process CSV 
+   
+    csv_bytes = process_csv
 
-    # Explode to rows
-    qa_df = df['Q&A'].explode().apply(pd.Series)
-    qa_df.columns = ['Question', 'Answer']
-
-    # Convert to CSV
-    csv_bytes = qa_df.to_csv().encode()
-
-    # Return CSV file
-    return send_file(
-        io.BytesIO(csv_bytes), 
-        mimetype='text/csv',
-        as_attachment=True,
-        attachment_filename='processed.csv'
-    )
+    return send_file(csv_bytes)
 
 def extract_qa(text):
-    pattern = r'\[Q\d+\. (.*)\]\s+(.*)'
-    return re.findall(pattern, text, re.DOTALL)
+   pattern = r'\[Q\d+\. (.*)\]\s+(.*)'
+   return re.findall(pattern, text, re.DOTALL)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
