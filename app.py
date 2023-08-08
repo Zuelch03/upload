810d99bd98bee438df061d3468b6498a1f6ad83c
Zuelch03 <gregzuelch316@gmail.com>
2023-08-08 10:33:03 -0700

    commit

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 import pandas as pd
 import re
 import os
+import tempfile
 
 app = Flask(__name__)
-
 
 def extract_qa(text):
     pattern = r'\[Q\d+\. (.*?)\]\s(.*?)(?=\s\[Q\d| $)'
     matches = re.findall(pattern, text, re.DOTALL)
     return matches
-
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -20,11 +19,12 @@ def upload_file():
 
         try:
             df['Q_and_A'] = df['add_text'].apply(extract_qa)
-            result_file_path = 'result.csv'
-            qna_df = pd.DataFrame(df['Q_and_A'].explode().tolist(), columns=[
-                                  'Question', 'Response'])
-            qna_df.to_csv(result_file_path, index=False)
-            message = 'File processed successfully. Download from '+result_file_path
+            with tempfile.NamedTemporaryFile(delete=False, suffix='.csv') as temp_file:
+                qna_df = pd.DataFrame(df['Q_and_A'].explode().tolist(), columns=['Question', 'Response'])
+                qna_df.to_csv(temp_file.name, index=False)
+                temp_file_path = temp_file.name
+
+            message = f'File processed successfully. <a href="/download?filename={os.path.basename(temp_file_path)}">Download result.csv</a>'
         except KeyError:
             message = "The column 'add_text' was not found in the provided CSV file."
 
@@ -32,6 +32,17 @@ def upload_file():
 
     return render_template('upload.html')
 
+@app.route('/download')
+def download_file():
+    filename = request.args.get('filename', '')
+    if filename:
+        try:
+            return send_from_directory(directory=tempfile.gettempdir(), filename=filename, as_attachment=True, download_name="result.csv")
+        except FileNotFoundError:
+            return "File not found."
+    else:
+        return "Invalid request."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
