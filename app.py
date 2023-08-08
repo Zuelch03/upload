64e7bd5131cd7399b7962ae7a2ab2dc51cc80178
Zuelch03 <85391955+Zuelch03@users.noreply.github.com>
2023-08-08 10:04:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, send_from_directory
 import pandas as pd
 import re
 import os
+import tempfile
 
 app = Flask(__name__)
 
@@ -18,11 +19,12 @@ def upload_file():
 
         try:
             df['Q_and_A'] = df['add_text'].apply(extract_qa)
-            result_file_path = 'result.csv'
-            qna_df = pd.DataFrame(df['Q_and_A'].explode().tolist(), columns=[
-                                  'Question', 'Response'])
-            qna_df.to_csv(result_file_path, index=False)
-            message = 'File processed successfully. <a href="/download">Download result.csv</a>'
+            with tempfile.NamedTemporaryFile(delete=False, suffix='.csv') as temp_file:
+                qna_df = pd.DataFrame(df['Q_and_A'].explode().tolist(), columns=['Question', 'Response'])
+                qna_df.to_csv(temp_file.name, index=False)
+                temp_file_path = temp_file.name
+
+            message = f'File processed successfully. <a href="/download?filename={os.path.basename(temp_file_path)}">Download result.csv</a>'
         except KeyError:
             message = "The column 'add_text' was not found in the provided CSV file."
 
@@ -32,11 +34,16 @@ def upload_file():
 
 @app.route('/download')
 def download_file():
-    try:
-        return send_from_directory(directory=os.getcwd(), filename="result.csv", as_attachment=True)
-    except FileNotFoundError:
-        return "File not found."
+    filename = request.args.get('filename', '')
+    if filename:
+        try:
+            return send_from_directory(directory=tempfile.gettempdir(), filename=filename, as_attachment=True, download_name="result.csv")
+        except FileNotFoundError:
+            return "File not found."
+    else:
+        return "Invalid request."
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
